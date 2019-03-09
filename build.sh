78cc655f1af86987591868a50e87f403edc975d4
Max Waugaman <mwaugaman@galois.com>
2019-03-08 17:06:04 -0800

    Bumped P3 to include first version of verilog and the galois P3 config. This config has not been modified for the GFE. Consider it a placeholder for now

--- build.sh
+++ build.sh
@@ -1,35 +1,55 @@
 #!/usr/bin/env bash
 
 function proc_usage {
-        echo "Usage: $0 [P1|P2]"
-        echo "Please specify a P1 or P2 processor!"
+    echo "Usage: $0 [P1|P2|P3]"
+    echo "Please specify a P1, P2, or P3 processor!"
 }
 
 function proc_picker {
-        # Parse the processor selection
-        if [ "$1" == "P1" ]; then
-                proc_name="P1"
-        elif [ "$1" == "P2" ]; then
-                proc_name="P2"
-        else
-                proc_usage
-                exit -1
-        fi
+    # Parse the processor selection
+    if [ "$1" == "P1" ]; then
+        proc_name="P1"
+    elif [ "$1" == "P2" ]; then
+        proc_name="P2"
+    elif [ "$1" == "P3" ]; then
+        proc_name="P3"
+    else
+        proc_usage
+        exit -1
+    fi
 }
 
 proc_picker $1
-cd rocket-chip/vsim
-make verilog PROJECT=galois.system CONFIG=${proc_name}FPGAConfig
-
-generated_dir=rocket-chip/vsim/generated-src
 user_ip_dir=$proc_name/xilinx_ip/hdl/
-if [[ $? -ne 0 ]]; then
-	echo "Processor build failed."
-	exit 1
-fi
 
-cd ../../
-cp $generated_dir/galois.system.${proc_name}FPGAConfig.v $user_ip_dir
-cp $generated_dir/galois.system.${proc_name}FPGAConfig.behav_srams.v $user_ip_dir
-cp $generated_dir/galois.system.${proc_name}FPGAConfig/AsyncResetReg.v $user_ip_dir
-cp $generated_dir/galois.system.${proc_name}FPGAConfig/plusarg_reader.v $user_ip_dir
+# Build Chisel P1 or P2
+if [ $proc_name == "P1" ] || [ $proc_name == "P2" ]; then
+    cd rocket-chip/vsim
+    make verilog PROJECT=galois.system CONFIG=${proc_name}FPGAConfig
+
+    generated_dir=rocket-chip/vsim/generated-src
+    if [[ $? -ne 0 ]]; then
+        echo "Processor build failed."
+        exit 1
+    fi
+
+    cd ../../
+    cp $generated_dir/galois.system.${proc_name}FPGAConfig.v $user_ip_dir
+    cp $generated_dir/galois.system.${proc_name}FPGAConfig.behav_srams.v $user_ip_dir
+    cp $generated_dir/galois.system.${proc_name}FPGAConfig/AsyncResetReg.v $user_ip_dir
+    cp $generated_dir/galois.system.${proc_name}FPGAConfig/plusarg_reader.v $user_ip_dir
+
+# Build Chisel P3
+else
+    cd P3/boom-template/verisim
+    # make PROJECT=boom.galois.system CONFIG=BoomP3FPGAConfig
+    if [[ $? -ne 0 ]]; then
+        echo "Processor build failed."
+        exit 1
+    fi
+
+    cd ../../../
+    generated_dir=P3/boom-template/verisim/generated-src
+
+    cp $generated_dir/boom.galois.system.TestHarness.BoomP3FPGAConfig.v $user_ip_dir
+fi
